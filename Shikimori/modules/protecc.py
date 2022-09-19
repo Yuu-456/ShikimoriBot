97478eb4760039d96659264c0baefdaadf0cc2b9
princessgirlxd <92134838+Princessgirlxd@users.noreply.github.com>
2022-09-19 16:38:25 +0530

    Update protecc.py

--- Shikimori/modules/protecc.py
+++ Shikimori/modules/protecc.py
@@ -5,9 +5,6 @@ import requests
 from asyncio import sleep
 from bs4 import BeautifulSoup
 
-from sqlalchemy.sql.waifu_sql import is_harem, add_grp, rm_grp, get_all_grp
-from sqlalchemy.sql.husb_sql import is_husb, add_hus_grp, rm_hus_grp, get_all_hus_grp
-from . import *
 
 qt = "Add them to your harem by sending"
 qt_bots = ["792028928", "1733263647"]
