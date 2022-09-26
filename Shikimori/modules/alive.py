3bae48b84ae3e07f5f34335b72af21c0bb7518d3
princessgirlxd <92134838+Princessgirlxd@users.noreply.github.com>
2022-09-26 13:23:03 +0530

    Update alive.py

--- Shikimori/modules/alive.py
+++ Shikimori/modules/alive.py
@@ -57,7 +57,7 @@ async def awake(update: Update, context: CallbackContext):
     await asyncio.sleep(0.1)
 first_name = update.effective_user.first_name
 user = message.from_user
-    TEXT = f"""
+    TEXT = f'''
     <b>Hi <a href="tg://user?id={user.id}">{first_name}</a>, I'm {bot_name} Robot.
 
 ⚪ I'm Working Properly
